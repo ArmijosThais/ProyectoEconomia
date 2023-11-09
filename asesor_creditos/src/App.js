9f4e8b7d15aff680ce4fa30d0ab98f497110f0fd
ArmijosThais <tarmijos02@gmail.com>
2023-11-09 07:35:59 -0500

    Cambios Header

--- asesor_creditos/src/App.js
+++ asesor_creditos/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+
+    </Router>
   );
 }
 
 export default App;
+
+/*
+<Switch>
+        <Route path="/pagina1" component={Page1} />
+        <Route path="/pagina2" component={Page2} />
+        <Route path="/" component={HomePage} />
+      </Switch>
+
+*/
---
+++ asesor_creditos/src/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Inicio</a>
+          </li>
+          <li>
+            <a href="/pagina1">Página 1</a>
+          </li>
+          <li>
+            <a href="/pagina2">Página 2</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
