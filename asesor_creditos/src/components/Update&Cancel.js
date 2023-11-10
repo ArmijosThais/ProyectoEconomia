e63766e2dfa6ab11e265f83be680b19bdbd3fa5e
Keybrish <keychanpa@gmail.com>
2023-11-10 01:27:48 -0500

    Vista de editar info institución

---
+++ asesor_creditos/src/components/Update&Cancel.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+
+function UpdateCancel({ action, cancel }) {
+  const color = '#ffdf00';
+  const [hovered, setHovered] = useState(false);
+  const backgroundColor = hovered ? lightenColor(color, 0.3) : color;
+
+  const handleHover = () => {
+    setHovered(true);
+  };
+
+  const handleLeave = () => {
+    setHovered(false);
+  };
+
+  function lightenColor(color, factor) {
+    const hexToRgb = (hex) =>
+      hex
+        .replace(/^#/, '')
+        .match(/.{2}/g)
+        .map((x) => parseInt(x, 16));
+
+    const rgb = hexToRgb(color);
+
+    const lightenedRgb = rgb.map((value) =>
+      Math.round(value + (255 - value) * factor)
+    );
+
+    const lightenedHex = `#${lightenedRgb
+      .map((x) => x.toString(16).padStart(2, '0'))
+      .join('')}`;
+
+    return lightenedHex;
+  }
+
+  return (
+    <div style={{ display: 'flex' }}>
+      <button
+        to="/login"
+        style={{
+          height: '40px',
+          width: '150px',
+          fontSize: '16px',
+          fontWeight: 'bold',
+          textDecoration: 'none',
+          color: 'black',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor,
+          borderRadius: '15px',
+          cursor: 'pointer',
+          transition: 'background-color 0.3s ease',
+          border: 'none',
+          marginTop: '15px',
+        }}
+        onMouseEnter={handleHover}
+        onMouseLeave={handleLeave}
+        onClick={action}
+      >
+        Actualizar
+      </button>
+      <button
+        style={{
+          height: '40px',
+          width: '150px',
+          fontSize: '16px',
+          fontWeight: 'bold',
+          textDecoration: 'none',
+          color: 'black',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: 'white',
+          borderRadius: '15px',
+          cursor: 'pointer',
+          transition: 'background-color 0.3s ease',
+          border: '1px solid black',
+          marginTop: '15px',
+          marginLeft: '25px',
+        }}
+        onClick={cancel}
+      >
+        Cancelar
+      </button>
+    </div>
+  );
+}
+
+export default UpdateCancel;
--- asesor_creditos/src/views/EditInstitution.js
+++ asesor_creditos/src/views/EditInstitution.js
@@ -1,7 +1,146 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Header from '../components/Header';
+import ArrowLeftLine from '../icons/arrowLeftLine';
+import EditIcon from '../icons/editIcon';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import UpdateCancel from '../components/Update&Cancel';
 
 function EditInstitution() {
-  return <div></div>;
+  const institutionName = 'Banco Pichincha';
+  const icon =
+    'https://upload.wikimedia.org/wikipedia/commons/c/cc/Banco-Pichincha.png';
+
+  const [color, setColor] = useState('#ffdf00'); // Valor inicial negro
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleColorChange = (event) => {
+    const inputValue = event.target.value;
+
+    // Verificar si el valor es un color hexadecimal válido
+    if (/^#[0-9A-Fa-f]{6}$/.test(inputValue)) {
+      setColor(inputValue);
+    } else {
+      // Puedes manejar la invalidación de alguna manera, como mostrar un mensaje de error
+      console.log('Ingrese un color hexadecimal válido');
+    }
+  };
+
+  return (
+    <div>
+      <Header iconSide={<ArrowLeftLine />} button={'Salir'} destination={'/'} />
+      <text
+        style={{
+          fontSize: '30px',
+          fontWeight: 'bold',
+          display: 'flex',
+          marginTop: '50px',
+          marginLeft: '122px',
+          marginRight: '50px',
+        }}
+      >
+        Información de la institución
+      </text>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <text
+          style={{
+            fontSize: '20px',
+            fontWeight: 'bold',
+            display: 'flex',
+            margin: '20px 30px 0px 122px',
+          }}
+        >
+          Nombre de la institución:
+        </text>
+        <Form.Control
+          type="text"
+          id="inputPassword5"
+          aria-describedby="passwordHelpBlock"
+          defaultValue={institutionName}
+          style={{
+            fontSize: '16px',
+            display: 'flex',
+            marginTop: '20px',
+            width: '300px',
+          }}
+        />
+      </div>
+      <div style={{ display: 'flex' }}>
+        <text
+          style={{
+            fontSize: '20px',
+            fontWeight: 'bold',
+            display: 'flex',
+            margin: '10px 217.5px 0px 122px',
+          }}
+        >
+          Logo:
+        </text>
+        <img
+          src={icon}
+          alt="Descripción de la imagen"
+          style={{ width: '150px', height: '150px', marginTop: '10px' }}
+          onClick={handleShow}
+        />
+      </div>
+      <div style={{ display: 'flex' }}>
+        <text
+          style={{
+            fontSize: '20px',
+            fontWeight: 'bold',
+            display: 'flex',
+            margin: '10px 213px 0px 122px',
+          }}
+        >
+          Color:
+        </text>
+        <Form.Control
+          type="color"
+          id="exampleColorInput"
+          defaultValue={color}
+          onChange={handleColorChange}
+          title="Choose your color"
+          style={{ marginTop: '10px' }}
+        />
+        <text
+          style={{
+            fontSize: '20px',
+            display: 'flex',
+            color: '#666666',
+            marginLeft: '15px',
+            marginTop: '10px',
+          }}
+        >
+          {color}
+        </text>
+      </div>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header>
+          <Modal.Title>Subir logo</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Control
+            type="text"
+            placeholder="Url de la imagen"
+            id="inputPassword5"
+            aria-describedby="passwordHelpBlock"
+            style={{
+              fontSize: '16px',
+              width: '465px',
+            }}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <UpdateCancel action={handleClose} cancel={handleClose} />
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 }
 
 export default EditInstitution;
