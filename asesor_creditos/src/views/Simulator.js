2f6e01ec89ad6a4d3690b2ac535f24500e086dbf
ArmijosThais <tarmijos02@gmail.com>
2023-11-14 11:03:49 -0500

    control de campos

--- asesor_creditos/src/views/Simulator.js
+++ asesor_creditos/src/views/Simulator.js
@@ -11,6 +11,7 @@ import MenuIcon from '../icons/menuIcon';
 import { obtenerCategorias } from '../services/apiCategorias';
 import { obtenerSubategorias } from '../services/apiSubcategorias';
 import Spinner from 'react-bootstrap/Spinner';
+import { toast } from 'react-toastify';
 
 function Simulator() {
   var nodes = [];
@@ -85,7 +86,6 @@ function Simulator() {
         (option) => option.nombreSubcategoria === event.target.text
       )
     );
-    console.log(selectedCredit?.montoMinimo);
   };
   const handleInputChange = (event, type) => {
     const value = event.target.value;
@@ -132,67 +132,92 @@ function Simulator() {
   }
 
   const generateTables = () => {
-    // Convertir el monto y los meses a números
-    const amount = parseFloat(amounti);
-    const months = parseInt(monthsi);
+    if (selectedType && selectedCredit) {
+      // Convertir el monto y los meses a números
+      const amount = parseFloat(amounti);
+      const months = parseInt(monthsi);
 
-    if (isNaN(amount) || isNaN(months) || amount <= 0 || months <= 0) {
-      console.error('Ingrese valores válidos para monto y meses.');
-      return;
-    }
+      if (
+        isNaN(amount) ||
+        isNaN(months) ||
+        amount <= selectedCredit.montoMinimo ||
+        months <= 0 ||
+        amount >= selectedCredit.montoMaximo ||
+        months >= selectedCredit.plazoMaximo
+      ) {
+        toast.error('Ingrese valores válidos para monto y meses.', {
+          position: 'top-right',
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+        });
+        return;
+      }
 
-    // Cálculos para amortización francesa
-    const rateFrancesa = selectedCredit.tasaInteres; // Tasa de interés anual (ejemplo)
-    console.log(selectedCredit.tasaInteres);
-    const monthlyRateFrancesa = rateFrancesa / 12 / 100; // Tasa de interés mensual
-    const monthlyPaymentFrancesa =
-      (amount * monthlyRateFrancesa) /
-      (1 - Math.pow(1 + monthlyRateFrancesa, -months));
+      // Cálculos para amortización francesa
+      const rateFrancesa = selectedCredit.tasaInteres; // Tasa de interés anual (ejemplo)
+      console.log(selectedCredit.tasaInteres);
+      const monthlyRateFrancesa = rateFrancesa / 12 / 100; // Tasa de interés mensual
+      const monthlyPaymentFrancesa =
+        (amount * monthlyRateFrancesa) /
+        (1 - Math.pow(1 + monthlyRateFrancesa, -months));
 
-    // Crear la tabla de amortización francesa
-    const nodes = [];
-    let remainingBalanceFrancesa = amount;
+      // Crear la tabla de amortización francesa
+      const nodes = [];
+      let remainingBalanceFrancesa = amount;
 
-    for (let i = 1; i <= months; i++) {
-      const interestFrancesa = remainingBalanceFrancesa * monthlyRateFrancesa;
-      const principalFrancesa = monthlyPaymentFrancesa - interestFrancesa;
+      for (let i = 1; i <= months; i++) {
+        const interestFrancesa = remainingBalanceFrancesa * monthlyRateFrancesa;
+        const principalFrancesa = monthlyPaymentFrancesa - interestFrancesa;
 
-      nodes.push({
-        id: i.toString(),
-        indirectChargesFee: monthlyPaymentFrancesa.toFixed(2).toString(),
-        interest: interestFrancesa.toFixed(2).toString(),
-        capital: principalFrancesa.toFixed(2).toString(),
-        balance: (remainingBalanceFrancesa -= principalFrancesa)
-          .toFixed(2)
-          .toString(),
+        nodes.push({
+          id: i.toString(),
+          indirectChargesFee: monthlyPaymentFrancesa.toFixed(2).toString(),
+          interest: interestFrancesa.toFixed(2).toString(),
+          capital: principalFrancesa.toFixed(2).toString(),
+          balance: (remainingBalanceFrancesa -= principalFrancesa)
+            .toFixed(2)
+            .toString(),
+        });
+      }
+
+      // Dividir el array nodes en dos partes
+      const firstPart = nodes.slice();
+      const secondPart = [];
+
+      // Cálculos para amortización alemana
+      const principalAlemana = amount / months;
+      let remainingBalanceAlemana = amount;
+
+      for (let i = 1; i <= months; i++) {
+        const interestAlemana = remainingBalanceAlemana * monthlyRateFrancesa;
+        const cuotaAlemana = principalAlemana + interestAlemana;
+
+        secondPart.push({
+          idd: i.toString(),
+          indirectChargesFeee: cuotaAlemana.toFixed(2).toString(),
+          interestt: interestAlemana.toFixed(2).toString(),
+          capitall: principalAlemana.toFixed(2).toString(),
+          balancee: (remainingBalanceAlemana -= principalAlemana)
+            .toFixed(2)
+            .toString(),
+        });
+      }
+
+      setData({ nodes: firstPart });
+      setDataAlemana({ nodes: secondPart });
+    } else {
+      toast.error('Complete todos los campos.', {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
       });
     }
-
-    // Dividir el array nodes en dos partes
-    const firstPart = nodes.slice();
-    const secondPart = [];
-
-    // Cálculos para amortización alemana
-    const principalAlemana = amount / months;
-    let remainingBalanceAlemana = amount;
-
-    for (let i = 1; i <= months; i++) {
-      const interestAlemana = remainingBalanceAlemana * monthlyRateFrancesa;
-      const cuotaAlemana = principalAlemana + interestAlemana;
-
-      secondPart.push({
-        idd: i.toString(),
-        indirectChargesFeee: cuotaAlemana.toFixed(2).toString(),
-        interestt: interestAlemana.toFixed(2).toString(),
-        capitall: principalAlemana.toFixed(2).toString(),
-        balancee: (remainingBalanceAlemana -= principalAlemana)
-          .toFixed(2)
-          .toString(),
-      });
-    }
-
-    setData({ nodes: firstPart });
-    setDataAlemana({ nodes: secondPart });
   };
 
   return (
@@ -330,21 +355,35 @@ function Simulator() {
             >
               Monto:
             </text>
-            <InputGroup style={{ width: '300px', height: '50px' }}>
-              <InputGroup.Text id="basic-addon1" style={{ height: '50px' }}>
-                $
-              </InputGroup.Text>
-              <Form.Control
-                style={{ height: '50px' }}
-                type="number"
-                step="0.01"
-                placeholder="00.00"
-                aria-label="decimal-input"
-                onChange={(event) => handleInputChange(event, 'amount')}
-                min={selectedCredit?.montoMinimo}
-                max={selectedCredit?.montoMaximo}
-              />
-            </InputGroup>
+            <div>
+              <InputGroup style={{ width: '300px', height: '50px' }}>
+                <InputGroup.Text id="basic-addon1" style={{ height: '50px' }}>
+                  $
+                </InputGroup.Text>
+                <Form.Control
+                  style={{ height: '50px' }}
+                  type="number"
+                  step="0.01"
+                  placeholder="00.00"
+                  aria-label="decimal-input"
+                  onChange={(event) => handleInputChange(event, 'amount')}
+                  min={selectedCredit?.montoMinimo}
+                  max={selectedCredit?.montoMaximo}
+                />
+              </InputGroup>
+              {selectedCredit && (
+                <text
+                  style={{
+                    fontSize: '15px',
+                    display: 'flex',
+                    marginRight: '97px',
+                  }}
+                >
+                  Entre {selectedCredit.montoMinimo} y
+                  {selectedCredit.montoMaximo}
+                </text>
+              )}
+            </div>
           </div>
         </div>
         <div>
@@ -363,20 +402,33 @@ function Simulator() {
             >
               Meses plazo:
             </text>
-            <InputGroup style={{ width: '300px' }}>
-              <Form.Control
-                style={{ height: '50px' }}
-                type="number"
-                step="1"
-                placeholder="0"
-                aria-label="integer-input"
-                onChange={(event) => handleInputChange(event, 'months')}
-                max={selectedCredit?.plazoMaximo}
-              />
-              <InputGroup.Text id="basic-addon2" style={{ height: '50px' }}>
-                meses
-              </InputGroup.Text>
-            </InputGroup>
+            <div>
+              <InputGroup style={{ width: '300px' }}>
+                <Form.Control
+                  style={{ height: '50px' }}
+                  type="number"
+                  step="1"
+                  placeholder="0"
+                  aria-label="integer-input"
+                  onChange={(event) => handleInputChange(event, 'months')}
+                  max={selectedCredit?.plazoMaximo}
+                />
+                <InputGroup.Text id="basic-addon2" style={{ height: '50px' }}>
+                  meses
+                </InputGroup.Text>
+              </InputGroup>
+              {selectedCredit && (
+                <text
+                  style={{
+                    fontSize: '15px',
+                    display: 'flex',
+                    marginRight: '97px',
+                  }}
+                >
+                  Máximo {selectedCredit.plazoMaximo}
+                </text>
+              )}
+            </div>
           </div>
           <button
             style={{
